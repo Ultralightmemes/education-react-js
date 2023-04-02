885932b8c4af5b8503ec9a651792cee11059287c
markkhramovich <mark.khramovich@digitalinspirationlabs.io>
2023-04-02 11:29:45 +0300

    Change lessons sidebar

--- src/components/LessonComponent.jsx
+++ src/components/LessonComponent.jsx
@@ -3,22 +3,30 @@ import {Context} from "../index";
 
 const LessonComponent = ({lesson}) => {
     const {store} = useContext(Context)
+    let className = 'flex w-full items-center p-2 text-base font-normal text-gray-900 rounded-lg' +
+        ' cursor-pointer'
+    if (store.lesson === lesson.index) {
+        className += ' bg-blue-500 hover:bg-blue-600'
+    }
+    else {
+        className += ' hover:bg-gray-100'
+    }
     return (
         <li>
-            <button>
-                <div onClick={() => store.setLesson(lesson.index)}
-                    className='flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white'
-                >
-                    <span className="ml-3">{lesson.title}</span>
-                    {/*{lesson.is_auto_done ? (*/}
-                    {/*    <span className="ml-auto mr-0">*/}
-                    {/*        {lesson.is_done ?*/}
-                    {/*            <input type="checkbox" className="object-right" disabled checked={is_done}/> :*/}
-                    {/*            <input type="checkbox" className="object-right" checked={is_done}*/}
-                    {/*                   onChange={formSubmitHandler} onClick={() => setIsDone(true)}/>}*/}
-                    {/*    </span>) : null}*/}
-                </div>
-            </button>
+            <div
+                onClick={() => store.setLesson(lesson.index)}
+                className={className}
+            >
+                <span className="ml-5">{lesson.title}</span>
+                {/*{lesson.is_auto_done ? (*/}
+                {/*    <span className="ml-auto mr-0">*/}
+                {/*        {lesson.is_done ?*/}
+                {/*            <input type="checkbox" className="object-right" disabled checked={is_done}/> :*/}
+                {/*            <input type="checkbox" className="object-right" checked={is_done}*/}
+                {/*                   onChange={formSubmitHandler} onClick={() => setIsDone(true)}/>}*/}
+                {/*    </span>) : null}*/}
+            </div>
+            <hr className="ml-3 border-gray-400 w-11/12"/>
         </li>
     );
 };
--- src/components/ThemeWithLessons.jsx
+++ src/components/ThemeWithLessons.jsx
@@ -5,9 +5,10 @@ const ThemeWithLessons = ({theme}) => {
     return (
         <li>
             <div
-                className='p-2 text-xl font-semibold text-gray-900 rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-blue-700 '>
+                className='p-2 text-xl font-semibold text-gray-900 rounded-lg'>
                 <span className="ml-1">{theme.title}</span>
             </div>
+            <hr className="border-gray-800"/>
             <ul className="relative">
                 {theme.lessons.map(lesson => <LessonComponent lesson={lesson} key={lesson.id}/>)}
             </ul>
