6cedaf39eea2120d4708966dd21e498ce3b14b99
markkhramovich <mark.khramovich@digitalinspirationlabs.io>
2023-04-11 18:59:27 +0300

    Change themes style

--- src/components/ThemeWithLessons.jsx
+++ src/components/ThemeWithLessons.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LessonComponent from "./LessonComponent";
 
 const ThemeWithLessons = ({theme}) => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <li>
             <div
-                className='p-2 text-xl font-semibold text-gray-900 rounded-lg'>
-                <span className="ml-1">{theme.title}</span>
+                className='p-2 text-l font-semibold text-gray-900 rounded-lg hover:bg-gray-300 cursor-pointer'
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <span>{theme.title}</span>
             </div>
             <hr className="border-gray-800"/>
-            <ul className="relative">
-                {theme.lessons.map(lesson => <LessonComponent lesson={lesson} key={lesson.id}/>)}
-            </ul>
+            {isOpen &&
+                <ul className="relative">
+                    {theme.lessons.map(lesson => <LessonComponent lesson={lesson} key={lesson.id}/>)}
+                </ul>
+            }
         </li>
     );
 };
