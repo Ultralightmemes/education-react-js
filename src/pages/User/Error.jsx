a219a53c35c0d601099ecfde751da40f197cbeef
markkhramovich <mark.khramovich@digitalinspirationlabs.io>
2023-04-11 22:15:25 +0300

    Add 404 page

--- src/pages/User/Error.jsx
+++ src/pages/User/Error.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
-            <h1 style={{color: "red"}}>
-                Вы перешли на несуществующую страницу
-            </h1>
+        <div className="text-center items-center justify-center">
+            <img className="mx-auto w-5/12"
+                src="/Lovepik_com-832506220-404%20white%20cloud%20error%20page%20art%20word%20simple%20wind%20can%20be%20commercia.png"/>
         </div>
     );
 };
